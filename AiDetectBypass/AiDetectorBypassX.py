b6dcf3b25c4e6e7b93e35ad8f66d980ba74eb649
GR1MR34P3R-1 <66636694+GR1MR34P3R-1@users.noreply.github.com>
2023-09-16 08:35:15 -0700

    AiDetectorBypassX.py

--- AiDetectBypass/AiDetectorBypassX.py
+++ AiDetectBypass/AiDetectorBypassX.py
@@ -8,6 +8,7 @@ from tkinter import ttk
 from nltk.corpus import wordnet, words as nltk_words
 from nltk.tokenize import sent_tokenize, word_tokenize
 from nltk.tag import pos_tag
+import language_tool_python
 
 nltk.download("punkt")
 nltk.download("wordnet")
@@ -16,6 +17,9 @@ nltk.download("averaged_perceptron_tagger")
 
 common_words_set = set(nltk_words.words())
 emphasis_words = ["very", "quite", "so", "too", "really"]
+
+# Create a LanguageTool object for grammar and spelling checking
+language_tool = language_tool_python.LanguageTool('en-US')
 
 # Function to get synonyms of a word using NLTK's WordNet
 def get_synonyms(word):
@@ -34,8 +38,7 @@ def paraphrase_sentence(sentence):
 
     for word in words:
         synonyms = get_synonyms(word)
-        if synonyms and random.randint(0, 100) < 20:  # Lower synonym replacement rate for subtlety
-            # Choose a synonym that fits the context
+        if synonyms and random.randint(0, 100) < 20:
             context_synonyms = [syn for syn in synonyms if syn in sentence]
             if context_synonyms:
                 synonym = random.choice(context_synonyms)
@@ -50,6 +53,7 @@ def paraphrase_sentence(sentence):
     paraphrased_sentence = " ".join(paraphrased_words)
     return paraphrased_sentence
 
+# Function to emphasize adjectives in the text
 def emphasize_adjectives(text, percent_to_change_adj):
     words = word_tokenize(text)
     tagged_words = pos_tag(words)
@@ -68,7 +72,6 @@ def emphasize_adjectives(text, percent_to_change_adj):
 
     emphasized_sentence = " ".join(output_words)
 
-    # Ensure that the emphasized sentence retains the original adjectives
     original_adjectives = [word for word, pos in tagged_words if pos.startswith('JJ')]
     emphasized_adjectives = [word for word, pos in pos_tag(emphasized_sentence.split()) if pos.startswith('JJ')]
     if original_adjectives and emphasized_adjectives:
@@ -79,13 +82,13 @@ def emphasize_adjectives(text, percent_to_change_adj):
 # Function to automatically calculate the percentage based on character count
 def calculate_percentage(char_count):
     if char_count <= 500:
-        return random.uniform(40, 60)
+        return random.uniform(20, 40)  # Adjusted percentage range for better results
     elif char_count <= 1000:
-        return random.uniform(30, 50)
+        return random.uniform(15, 30)  # Adjusted percentage range
     elif char_count <= 2000:
-        return random.uniform(20, 40)
+        return random.uniform(10, 20)  # Adjusted percentage range
     else:
-        return random.uniform(10, 30)
+        return random.uniform(5, 15)   # Adjusted percentage range
 
 # Function to handle text modification while avoiding plagiarism
 def modify_text():
@@ -96,17 +99,20 @@ def modify_text():
         return
 
     try:
-        char_count = len(input_text)
-        percent_to_change_syn = calculate_percentage(char_count)
-        percent_to_change_adj = calculate_percentage(char_count)  # Automatically calculated for adjectives
+        # Perform spelling and grammar checking
+        corrected_text = language_tool.correct(input_text)
 
-        paragraphs = sent_tokenize(input_text)
+        char_count = len(corrected_text)
+        percent_to_change_syn = calculate_percentage(char_count)
+        percent_to_change_adj = calculate_percentage(char_count)
+
+        paragraphs = sent_tokenize(corrected_text)
         modified_paragraphs = []
 
         for paragraph in paragraphs:
             # Preserve formatting elements (numbers, abbreviations, and bold text)
             preserved_paragraph = re.sub(r'(\d+\.)|([A-Z]+\.)', r' \1\2 ', paragraph)
-            preserved_paragraph = re.sub(r'\*\*(.*?)\*\*', r'**\1**', preserved_paragraph)
+            preserved_paragraph = re.sub(r'"(.*?)"', r'"\1"', preserved_paragraph)  # Replace double quotes
 
             # Paraphrase the sentence while avoiding plagiarism
             paraphrased_sentence = paraphrase_sentence(preserved_paragraph)
@@ -114,52 +120,52 @@ def modify_text():
             # Emphasize adjectives in the paraphrased sentence
             emphasized_sentence = emphasize_adjectives(paraphrased_sentence, percent_to_change_adj)
 
+            # Remove extra spaces before punctuation
+            emphasized_sentence = re.sub(r'\s+([.,;!?])', r'\1', emphasized_sentence)
+
             modified_paragraphs.append(emphasized_sentence)
 
         modified_text = '\n'.join(modified_paragraphs)
+
+        # Replace double quotes with straight quotes
+        modified_text = modified_text.replace("“", '"').replace("”", '"')
+
         output_text_widget.delete("1.0", tk.END)
         output_text_widget.insert(tk.END, modified_text)
 
         messagebox.showinfo("Success", "Text modification completed successfully.")
 
-    except ValueError:
-        messagebox.showinfo("Error", "Invalid input for percentages.")
     except Exception as e:
         messagebox.showinfo("Error", f"An error occurred: {str(e)}")
 
 # Function to perform text modification in a separate thread
 def modify_text_thread():
-    # Disable the modify button to prevent concurrent modifications
     modify_button.config(state=tk.DISABLED)
 
     try:
-        modify_text()  # Call your existing modify_text function
+        modify_text()
 
-        # Re-enable the modify button when modification is done
         modify_button.config(state=tk.NORMAL)
-        progress_bar.stop()  # Stop the progress bar when done
+        progress_bar.stop()
     except Exception as e:
         messagebox.showinfo("Error", f"An error occurred: {str(e)}")
 
 # Function to trigger text modification using threading
 def start_modification_thread():
-    # Create a new thread to perform text modification
     modification_thread = threading.Thread(target=modify_text_thread)
-
-    # Start the thread
     modification_thread.start()
-    progress_bar.start()  # Start the progress bar
+    progress_bar.start()
 
-# Create the main window
 window = tk.Tk()
 window.title("Text Modifier")
 
-# Create and place UI elements
 input_label = tk.Label(window, text="Input Text:")
 input_label.pack()
 
+# Set focus to the input text box when the window opens
 input_text_widget = scrolledtext.ScrolledText(window, wrap=tk.WORD, width=40, height=10)
 input_text_widget.pack()
+input_text_widget.focus_set()  # Set focus here
 
 percent_adj_label = tk.Label(window, text="Auto-Calculated Percentage of Adjective Emphasis:")
 percent_adj_label.pack()
@@ -173,9 +179,7 @@ output_label.pack()
 output_text_widget = scrolledtext.ScrolledText(window, wrap=tk.WORD, width=40, height=10)
 output_text_widget.pack()
 
-# Create a progress bar
 progress_bar = ttk.Progressbar(window, mode="indeterminate")
 progress_bar.pack()
 
-# Start the main loop
 window.mainloop()
